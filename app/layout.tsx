7084afc35f1315c34729d6818c570f6441ee8807
Austin Serb <serbaustin@gmail.com>
2024-08-26 22:44:17 -0700

    app(Vercel): - Added Speed Insights and Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -30,6 +31,7 @@ export default function RootLayout({
         </nav>
         {children}
         <Analytics/>
+        <SpeedInsights/>
         <Footer />
       </body>
     </html>
